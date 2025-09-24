596020f72660bf7351a78307f40e56af37da7598
Zahaawii <zahaawii@gmail.com>
2025-09-24 09:13:23 +0200

    init project

---
+++ src/main/java/com/example/projekt1frontend/Projekt1FrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.projekt1frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt1FrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projekt1FrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projekt1frontend/Projekt1FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projekt1frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt1FrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
